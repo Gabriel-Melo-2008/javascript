65402e57f42af68cb520d8c78ff3f467b58e8447
Gabriel Melo <gabrielsistemas2024@gmail.com>
2025-06-18 16:35:31 -0300

    4jktrkçj5hnk
    j4gvçkrnç

---
+++ aula5/index.js
@@ -0,0 +1,13 @@
+/*var vel = 100
+console.log(`A velociadade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você utrapassou o limite da velocidade permitida. Multado!!!`)
+}
+console.log('Dirija com o cinto de segurança')*/
+
+var pais = 'BrasiL'
+if(pais.toLowerCase() == 'brasil'){
+    console.log('Você é brasileiro')
+}else{
+    console.log('Você é estrangeiro')
+}
