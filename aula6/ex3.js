d3c34a36fd6e530091b4afb4628a71997661a449
Gabriel Melo <gabrielsistemas2024@gmail.com>
2025-07-06 18:07:16 -0300

    oi
    fjbuf

---
+++ aula6/ex3.js
@@ -0,0 +1,25 @@
+agora = new Date()
+diasem = agora.getDay()
+switch(diasem){
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Domingo')
+        break
+}
